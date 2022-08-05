86f2b65c61a96efa246aac3d24e235b35e69fa17
Akashappanna <akash@discovermyfranchise.com>
2022-08-05 18:41:52 +0530

    new feature added

--- src/components/Netflix/Header/Header.js
+++ src/components/Netflix/Header/Header.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import logo from "../../../assests/NetFlixLogo.png";
 import { FaSearch, FaBell } from "react-icons/fa";
 import profile from "../../../assests/NetflixAvatar.png";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const [checkIcon,setCheckIcon]=useState(false)
+  const [checkIcon, setCheckIcon] = useState(false);
 
   const changeBg = () => {
     if (window.pageYOffset > 300) {
@@ -23,15 +23,16 @@ const Header = () => {
     };
   }, []);
 
-  const checkProfileHandler=()=>{
-    setCheckIcon(prevState => !prevState);
-  }
+  const checkProfileHandler = () => {
+    setCheckIcon((prevState) => !prevState);
+  };
 
   return (
     <div
-      className={`flex flex-row justify-between items-center px-4 py-2 w-full m-auto fixed z-10 ${
+      className={`flex flex-row justify-between items-center px-4 py-2 w-full m-auto fixed ${
         isVisible && "bg-black"
       }`}
+      style={{ zIndex: "110" }}
     >
       <div className="flex flex-row justify-start items-center w-3/5">
         <img src={logo} alt="logo" className="w-20 md:w-30 lg:w-40 " />
@@ -47,19 +48,57 @@ const Header = () => {
         <FaBell className="w-10 fill-white" />
         {/* <img src={profile} alt="logo" className="w-1/6" onClick={checkProfileHandler}/> */}
         <div className="relative inline-block text-left w-1/6">
-          <img src={profile} alt="logo"  onClick={checkProfileHandler}/>
-          {checkIcon && <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-            <div className="py-1" role="none">
-              <Link to='/' className="text-gray-700 block px-4 py-2 hover:bg-black hover:text-white text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</Link>
-              <Link to='/' className="text-gray-700 block px-4 py-2 text-sm hover:bg-black hover:text-white" role="menuitem" tabindex="-1" id="menu-item-1">Support</Link>
-              <Link to='/' className="text-gray-700 block px-4 py-2 text-sm hover:bg-black hover:text-white" role="menuitem" tabindex="-1" id="menu-item-2">License</Link>
-              <Link to='/'>
-                <button type="submit" className="text-gray-700 block w-full hover:bg-black hover:text-white text-left px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">
-                  Sign out
+          <img src={profile} alt="logo" onClick={checkProfileHandler} />
+          {checkIcon && (
+            <div
+              className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+              tabindex="-1"
+            >
+              <div className="py-1" role="none">
+                <Link
+                  to="/"
+                  className="text-gray-700 block px-4 py-2 hover:bg-black hover:text-white text-sm"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-0"
+                >
+                  Account settings
+                </Link>
+                <Link
+                  to="/"
+                  className="text-gray-700 block px-4 py-2 text-sm hover:bg-black hover:text-white"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-1"
+                >
+                  Support
+                </Link>
+                <Link
+                  to="/"
+                  className="text-gray-700 block px-4 py-2 text-sm hover:bg-black hover:text-white"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="menu-item-2"
+                >
+                  License
+                </Link>
+                <Link to="/">
+                  <button
+                    type="submit"
+                    className="text-gray-700 block w-full hover:bg-black hover:text-white text-left px-4 py-2 text-sm"
+                    role="menuitem"
+                    tabindex="-1"
+                    id="menu-item-3"
+                  >
+                    Sign out
                   </button>
-              </Link>
+                </Link>
+              </div>
             </div>
-          </div>}
+          )}
         </div>
       </div>
       {/* src="blob:https://www.netflix.com/d34809d8-69b8-497c-a528-3a4d31fc4461" */}
@@ -71,8 +110,7 @@ const Header = () => {
           />
         </video>
       </div> */}
-      </div>
-
+    </div>
   );
 };
 
